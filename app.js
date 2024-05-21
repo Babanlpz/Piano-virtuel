2ca45f5c35e33ae6f8ee6cb1019ce50365e558b6
Esteban <baab2an@live.fr>
2024-05-21 21:40:14 +0200

    FEAT note pour les functions

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const notes = document.querySelectorAll(".note");
 
 const volume = document.querySelector(".volume input");
 
+// Array to store all the notes
 let allAudios = [],
   audio = new Audio("./audios/a.way");
 
+// Function to play the piano
 const pianoPlay = (note) => {
   audio.src = `./audios/${note}.wav`;
   audio.play();
 
+  // Add active class to the key
   const clickKey = document.querySelector(`[data-key="${notes}"]`);
 
   clickKey.classList.add("active");
@@ -16,10 +19,11 @@ const pianoPlay = (note) => {
     clickKey.classList.remove("active");
   }, 150);
 };
-
+// Loop through all the notes
 notes.forEach((note) => {
   allAudios.push(note.dataset.key);
 
+  // Add click event to the notes
   note.addEventListener("click", () => pianoPlay(note.dataset.key));
 });
 
@@ -29,6 +33,7 @@ const pressKey = (e) => {
 
 document.addEventListener("keydown", pressKey);
 
+// Function to slide the volume
 function slideVolume(e) {
   audio.volume = e.target.value;
 }
